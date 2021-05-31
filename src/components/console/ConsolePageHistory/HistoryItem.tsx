5c7645afed377acfeca1639d737712cf8d1cf682
maks88sgt <maks88sgt>
2021-06-01 01:59:02 +0300

    Dropdown improvement

--- src/components/console/ConsolePageHistory/HistoryItem.tsx
+++ src/components/console/ConsolePageHistory/HistoryItem.tsx
@@ -4,6 +4,7 @@ import red from './../../../icons/red.svg';
 import green from './../../../icons/green.svg';
 import {HistoryItemDropdown} from "./HistoryItemDropdown";
 import ReactDOM from 'react-dom';
+import {Portal} from "./Portal";
 
 export type HistoryItemPropsType = {
     name: string;
@@ -14,27 +15,47 @@ export type HistoryItemPropsType = {
 export const HistoryItem = (props: HistoryItemPropsType) => {
     const { name, success, index } = props;
 
-    const [width, setWidth] = useState(null);
-    const div = useCallback(node => {
-        if (node !== null) {
-            setWidth(node.getBoundingClientRect().width);
-        }
-    }, []);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        document.body.addEventListener("click", event => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setIsDropdownOpen(false);
+        });
+        document.body.addEventListener("scroll", () => {
+            setIsDropdownOpen(false);
+        });
+    }, [])
 
     const [ isDropDownOpen, setIsDropdownOpen ] = useState(false);
-    const container = document.querySelector(`.historyItem--${index}`);
-    const portal = container && ReactDOM.createPortal(<HistoryItemDropdown/>, container);
-    const dropDown = isDropDownOpen && portal;
-    //const dropDown = isDropDownOpen && <HistoryItemDropdown/>;
+    const [ dropDownCoords, setDropDownCoords ] = useState({left: 0, top: 0});
+    const [ dropDownWidth, setDropDownWidth ] = useState(0);
 
-    return (<div ref={div} className={`historyItem_container`} onClick={()=>setIsDropdownOpen(!isDropDownOpen)}>
+    function handleDropDownClick(event) {
+        setIsDropdownOpen(!isDropDownOpen);
+        if (ref.current) {
+            setDropDownWidth(ref.current.offsetWidth );
+            const coords = ref.current.getBoundingClientRect();
+            setDropDownCoords({left: coords.left - 25, top: coords.top + coords.height});
+        }
+
+    }
+
+    return (<div ref={ref} className={`historyItem_container`}
+                 onClick={(event)=> handleDropDownClick(event)}>
         <div className={`historyItem--${index}`}>
             <div className={`historyItem`}>
                 <img src={success ? green : red}/>
                 <div className={'historyItem_name'}>{name}</div>
                 <img src={dots} alt={'actions'}/>
+                {isDropDownOpen && <Portal querySelector={`.historyItem--${index}`}>
+                    <HistoryItemDropdown dropDownCoords={dropDownCoords} dropDownWidth={dropDownWidth}/>
+                </Portal>
+                }
             </div>
-            {dropDown}
+
         </div>
 
     </div>);
--- src/components/console/ConsolePageHistory/HistoryItemDropdown.tsx
+++ src/components/console/ConsolePageHistory/HistoryItemDropdown.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-export const HistoryItemDropdown = () => {
+export type HistoryItemDropdownPropsType= {
+    dropDownCoords: {
+        top: number,
+        left: number,
+    };
+    dropDownWidth: number;
+}
+
+export const HistoryItemDropdown = (props: HistoryItemDropdownPropsType) => {
+    const { dropDownCoords, dropDownWidth } = props;
+
+    const position = {
+        width: dropDownWidth,
+        top: dropDownCoords.top,
+        left: dropDownCoords.left,
+    }
 
     return (
-        <div className={'historyItemDropdown'}>
+        <div className={'historyItemDropdown'} style={position}>
             <span className={'historyItemDropdown--run'}>Run</span>
             <span className={'historyItemDropdown--copy'}>Copy</span>
             <span className={'historyItemDropdown--delete'}>Delete</span>
